33098d1392ae4fe70c3fc65ce1e5612cf9b305a4
solOcampo <soolangeeliizabet@gmail.com>
2022-06-16 15:01:35 -0300

    Subo mi trabajo practico de Mercado 'liebre' con lo visto de html

---
+++ app.js
@@ -0,0 +1,9 @@
+const express=require('express')
+const path=require ('path')
+const app=express()
+const port =3000
+
+app.use(express.static(path.join(__dirname,'public')))
+app.get('/',(req,res)=> res.sendFile(path.resolve(__dirname,'views','home.html')))
+
+app.listen(port,()=>console.log(`Servidor levantado con exito en el puerto ${port}`));
